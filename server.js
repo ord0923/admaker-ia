a743d0a801e93a543738384223e232583323e08d
ord0923 <ord0923@gmail.com>
2026-09-07 21:32:26 -0500

    Update server.js

--- server.js
+++ server.js
@@ -100,6 +100,10 @@ app.post('/api/generate-image', async (req,res)=>{
 
 app.get('/api/campaigns',(req,res)=>res.json(demoCampaigns));
 
-app.get('*',(req,res)=>res.sendFile(path.join(__dirname,'public','index.html')));
-app.listen(port,()=>console.log(`AdMaker IA Pro: http://localhost:${port}`));
+app.use((req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.listen(port, '0.0.0.0', () => {
+  console.log(`AdMaker IA running on port ${port}`);
 });
